e49878ad36b050e48c4bc549e15e84a27e9409a7
Casey Brock <casey.brock613@gmail.com>
2022-11-13 00:07:22 -0500

    Add Instructions - update game class to use player names - update move input to check the user following input instructions - move player 1 and player 2 assignment in game class to own method

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,22 +1,67 @@
 # initialize a new game
 
+class Instructions
+  def self.introduction
+    puts 'Welcome To Tic-Tac-Toe!'
+    puts 'In case you need a refresher on how to play:'
+    puts 'The goal of the game is to get 3 of your letter (X or O) in a row before the other player.'
+    puts 'The row can be across, down or diagonal!'
+    puts 'The turn order will be randomly selected. On your turn you will be prompted to enter a move:'
+  end
+
+  def self.input_instructions
+    puts 'The three rows are labeled top, middle, and bottom'
+    puts 'The columns are labed left, middle, an right'
+    puts 'To place your letter, type in the row, then the column, seperated by a comma.'
+    puts 'For example, if you wanted to place a letter in the middle of the board, tou would enter "middle-middle'
+  end
+
+  def self.start
+    introduction
+    input_instructions
+    puts 'Are you ready to begin? (Y/N)'
+    confirmation = gets.chomp.downcase
+    until confirmation == 'y'
+      puts 'No problem, type "y" when you are ready. or to exit the game type "quit"'
+      confirmation = gets.chomp.downcase
+      return if cofirmation == 'quit' 
+    end
+  end
+end
 # create user player
 class Player
+  attr_reader :name
+
   def initialize
     puts 'Please enter your name'
     @name = gets.chomp
   end
 
   def take_turn
-    puts 'Please enter your move'
-    gets.chomp.downcase.split('-')
+    puts 'Please enter your move:'
+    check_input(gets.chomp.downcase.split('-'))
+  end
+
+  def check_input(move)
+    until (move[0] == 'top' || move[0] == 'middle' || move[0] == 'bottom') &&
+          (move[1] == 'left' || move[1] == 'middle' || move[1] == 'right') 
+      puts 'Please try again. As a reminder here are the input instructions:'
+      Instructions.input_instructions
+      move = gets.chomp.downcase.split('-')
+    end
+    move
   end
 end
-
 # AI player - Single Purpose decide computers moves
 class Computer
+  attr_reader :name
+
+  def initialize
+    @name = "The Computer"
+  end
+
   def take_turn
-    p [decide_row, decide_column]
+    [decide_row, decide_column]
   end
 
   private
@@ -81,45 +126,57 @@ class GameBoard
 
   def diagonal_win
     win_conditions = [[board[0][0], board[1][1], board[2][2]], [board[0][2], board[1][1], board[2][0]]]
-    @winner = 1 if win_conditions[0].all?('X') || win_conditions[1].all?('X')
-    @winner = 2 if win_conditions[0].all?('O') || win_conditions[1].all?('O')
+    @winner = 1 if win_conditions.any?(['X','X','X'])
+    @winner = 2 if win_conditions.any?(['O','O','O'])
   end
 
   def column_win
-    if board[0][0] == 'X' && board[1][0] == 'X' && board[2][0] == 'X' ||
-       board[0][1] == 'X' && board[1][1] == 'X' && board[2][1] == 'X' ||
-       board[0][2] == 'X' && board[1][2] =='X' && board[2][2] == 'X'
+    if [board[0][0], board[1][0], board[2][0]].all?("X") ||
+       [board[0][1], board[1][1], board[2][1]].all?("X") ||
+       [board[0][2], board[1][2], board[2][2]].all?("X")
       @winner = 1
-    elsif board[0][0] == 'O' && board[1][0] == 'O' && board[2][0] == 'O' ||
-          board[0][1] == 'O' && board[1][1] == 'O' && board[2][1] == 'O' ||
-          board[0][2] == 'O' && board[1][2] == 'O' && board[2][2] == 'O'
+    elsif [board[0][0], board[1][0], board[2][0]].all?("O") ||
+          [board[0][1], board[1][1], board[2][1]].all?("O") ||
+          [board[0][2], board[1][2], board[2][2]].all?("O")
       @winner = 2
     end
   end
 end
 
-
 class Game
   def initialize
-    who_goes_first = rand(1..2)
-    if who_goes_first == 1
-      @player1 = Player.new
-      @player2 = Computer.new
-    else
-      @player1 = Computer.new
-      @player2 = Player.new
-    end
+    Instructions.start
+    choose_first_player
     @game_board = GameBoard.new
     play_game
   end
 
-  def request_turn(player, letter)
-    send_turn_to_board(player.take_turn, letter)
-  end
-
   private
 
-  def send_turn_to_board(turn, letter)
+  def choose_first_player
+    if rand(1..2) == 1
+        @player1 = Player.new
+        @player2 = Computer.new
+      else
+        @player1 = Computer.new
+        @player2 = Player.new
+      end
+      puts "#{@player1.name} is player 1!"
+  end
+
+  def request_turn(player, letter)
+    loop do
+      move = player.take_turn
+      spot_on_board = convert_turn_to_board_space(move)
+      if @game_board.board[spot_on_board[0]][spot_on_board[1]] == ' '
+        @game_board.board[spot_on_board[0]][spot_on_board[1]] = letter
+        break
+      end
+      puts 'Space is not available. Please choose another move:' if player.instance_of?(Player)
+    end
+  end
+
+  def convert_turn_to_board_space(turn)
     case turn[0]
     when 'top'
       turn[0] = 0
@@ -136,17 +193,18 @@ class Game
     when 'right'
       turn[1] = 2
     end
-    @game_board.board[turn[0]][turn[1]] = letter
+    turn
   end
 
   def play_game
     while @game_board.check_for_winner.zero?
-      request_turn(@player1, 'X')
       @game_board.display_board
+      request_turn(@player1, 'X')      
       break unless @game_board.check_for_winner.zero?
 
       puts "Player 2's turn"
       request_turn(@player2, 'O')
+
     end
     display_winner
   end
@@ -160,4 +218,4 @@ class Game
   end
 end
 
-new_game = Game.new
+Game.new
